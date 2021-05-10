212769403244aac77d205efcc72af27da0727726
keyhanbabazadeh <keyhanbabazadeh@hotmail.com>
2021-05-10 14:10:29 +0430

    Output Codec added

--- SharpCommunication.GY955/Codec/Output.cs
+++ SharpCommunication.GY955/Codec/Output.cs
@@ -11,9 +11,26 @@ namespace SharpCommunication.GY955.Codec
         public AccuratedVector Accelerator { get; set; }
         public AccuratedVector Magnetometer { get; set; }
         public AccuratedVector Gyroscope { get; set; }
-        public Vector3 Euler { get; set; }
-        public Vector4 Quaternion { get; set; }
+        public Vector3? Euler { get; set; }
+        public Vector4? Quaternion { get; set; }
         public Level SystemAccuracy { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            if (Types.HasFlag(VectorDataType.Accelerator) && Accelerator != null)
+                sb.Append($"Accelerator {{ X : {Accelerator.Value.X}, Y : {Accelerator.Value.Y}, Z : {Accelerator.Value.Z}, ");
+            if (Types.HasFlag(VectorDataType.Gyroscope) && Gyroscope != null)
+                sb.Append($"Gyroscope {{ X : {Gyroscope.Value.X}, Y : {Gyroscope.Value.Y}, Z : {Gyroscope.Value.Z}, ");
+            if (Types.HasFlag(VectorDataType.Magnetometer) && Magnetometer != null)
+                sb.Append($"Magnetometer {{ X : {Magnetometer.Value.X}, Y : {Magnetometer.Value.Y}, Z : {Magnetometer.Value.Z}, ");
+            if (Types.HasFlag(VectorDataType.Euler) && Euler != null)
+                sb.Append($"Euler {{ X : {Euler.Value.X}, Y : {Euler.Value.Y}, Z : {Euler.Value.Z}, ");
+            if (Types.HasFlag(VectorDataType.Quaternion) && Quaternion != null)
+                sb.Append($"Quaternion {{ X : {Quaternion.Value.X}, Y : {Quaternion.Value.Y}, Z : {Quaternion.Value.Z}, ");
+            return sb.ToString();
+
+        }
 
     }
 
